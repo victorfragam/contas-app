ac34dda0bff94c5fcacf3c945eddbcf11c9662bb
victorfragam <victorfragam@gmail.com>
2026-06-22 00:45:45 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v3';
+const VERSION = 'v4';
 const APP = 'contas-' + VERSION;
 const CDN = 'contas-cdn-v1';
 const CORE = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png', '/favicon.png'];
